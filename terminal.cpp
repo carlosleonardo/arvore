a224375cdc4107d05a4f1f8718f82826dfee0426
Carlos Leonardo Goes Farias <carlos.leonardo.goes@gmail.com>
2026-08-10 15:34:22 -0300

    Alteração para que terminal seja usado apenas no Windows

--- terminal.cpp
+++ terminal.cpp
@@ -3,9 +3,14 @@
 // Created by S861321135 on 04/08/2026.
 //
 #include "terminal.h"
+
+#ifdef _WIN32
 #include <windows.h>
+#endif
 
 void terminal_initialize() {
+#ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+#endif
 }
